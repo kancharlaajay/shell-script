72fd44f0c54e2b683baa04f5f79a2c476493e508
kancharlaajay <kancharlaajay1234@gmail.com>
2025-05-19 18:44:07 +0530

    name added

---
+++ 05-variables-values-reading.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "What is your birthday date::"
+read -s date
+echo "Entered Date: $date"
+
+echo "What is your birthday month::"
+read -s month
+echo "Entered Date: $month"
+
+echo "What is your birthday year::"
+read -s year
+echo "Entered Date: $year"
