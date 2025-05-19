5eb9fa42fa035030fe8b952c81682b095ae7efe3
kancharlaajay <kancharlaajay1234@gmail.com>
2025-05-19 18:51:09 +0530

    name added

---
+++ 06-arthmetic-variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+Num1=10
+Num2=20
+
+sum=$((Num1+Num2))
+echo "Sum: $sum"
+
+difference=$((Num2-Num1))
+echo "Difference: $difference"
+
+product=$((Num1*Num2))
+echo "Product: $product"
+
+quotient=$((Num2/Num1))
+echo "Quotient: $quotient"
+
