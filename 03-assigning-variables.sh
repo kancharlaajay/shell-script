ec011183d7e6b3760cad8268b81763ee235405b3
kancharlaajay <kancharlaajay1234@gmail.com>
2025-05-19 18:24:21 +0530

    name added

---
+++ 03-assigning-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Ajay
+Person2=Vamsi
+
+echo "$Person1:: Hi $Person2"
+echo "$Person2:: Hello $Person1"
+echo "$Person1:: How are you doing today $Person2"
+echo "$Person2:: I'm doing good, hope you the same $Person1"
