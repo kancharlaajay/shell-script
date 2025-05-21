2bc62a1bdf7ed8baeec3bcff67cca4464dadd841
kancharlaajay <kancharlaajay1234@gmail.com>
2025-05-21 18:25:11 +0530

    datatype script added

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -4,6 +4,7 @@ NUMBER1=$1
 NUMBER2=$2
 
 TIMESTAMP=$(date)     # Here, date is the linux command, we want to execute any through script, need to use $(<command-name>)
+echo "Script executed at: $TIMESTAMP"
 
 SUM=$(($NUMBER1+NUMBER2))
 echo "Sum of $NUMBER1 and $NUMBER2: $SUM"
